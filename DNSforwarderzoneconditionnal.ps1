f936ab5c17af30d00f6953c3735cf70985505a83
Satcom <47340064+Satcomx00-x00@users.noreply.github.com>
2020-10-14 10:02:30 +0200

    Add files via upload

---
+++ DNSforwarderzoneconditionnal.ps1
@@ -0,0 +1,7 @@
+﻿cd C:\Users\$env:UserName\Desktop
+$csvfile = Import-Csv .\2020_m3104_plan_lab.csv -Delimiter ';'
+foreach($line in $csvfile){
+    Add-DnsServerConditionalForwarderZone -Name $line.vdportgroup -MasterServers $line.ipv6_gu_routeur +","+ $line.ipv4_svr -PassThru
+    echo " --------------Add-------------- " $line.vdportgroup "Server on IPV4/IPV6" $line.ipv4_svr
+}
+Get-DnsServerForwarder
